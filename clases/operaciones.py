f0cd354c9d84927882bc11920385b1c57331d1e5
DHorner14 <194024-7@iberoleon.edu.mx>
2025-09-04 12:16:12 -0600

    VEr i

---
+++ clases/operaciones.py
@@ -0,0 +1,46 @@
+class Operaciones:
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+        self.resultado = 0
+        
+    def leerNumeros(self):
+        while True:
+            try:
+                self.num1 = int(input("Número 1:"))
+                break
+            except Exception:
+                print("Número inválido")
+                continue
+        while True:
+            try:
+                self.num2 = int(input("Número 2:"))
+                break
+            except Exception:
+                print("Número inválido")
+                continue    
+    
+    def sumar(self):
+        self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igula a " + str(self.num1 + self.num2)
+    
+    def restar(self):
+        self.resultado = "La resta de " + str(self.num1) + " - " + str(self.num2) + " es igula a " + str(self.num1 - self.num2)
+        
+    def multiplicar(self):
+        self.resultado = "La multiplicación de " + str(self.num1) + " * " + str(self.num2) + " es igula a " + str(self.num1 * self.num2)  
+    
+    def dividir(self):
+        if self.num2 == 0:
+            self.resultado = "No se puede dividir por cero"
+        else:
+            self.resultado = "La división de " + str(self.num1) + " / " + str(self.num2) + " es igula a " + str(self.num1 / self.num2)  
+    
+    def modulo(self):
+        if self.num2 == 0:
+            self.resultado = "No se puede dividir por cero"
+        else:
+            self.resultado = "El módulo de " + str(self.num1) + " % " + str(self.num2) + " es igula a " + str(self.num1 % self.num2)
+    def mostrarResultado(self):
+        print(self.resultado)
+        
+        
